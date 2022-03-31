1f6cebf5becee96a2af40cd6e0c93067b1a09679
ricardovalley <ricardo.v.valadas@gmail.com>
2022-03-31 09:47:49 +0200

    Data is extracted into a dictionary. Started implementing the data split into training and testing

--- dataset/helper.py
+++ dataset/helper.py
@@ -1,13 +1,41 @@
 import pandas as pd
 import numpy as np
+import torch
 
 import os
     
 DIR = os.path.dirname(os.path.abspath(__file__))
 DATA = os.path.join(DIR, "sample_data")
 
-def sample_input():
-    x = pd.read_csv(os.path.join(DATA, "user_0001/user_0001_highway.csv"))
+def sample_input(filename):
+    x = pd.read_csv(os.path.join(DATA, filename))
     x = x.drop(columns=["Unnamed: 0"])
     x = x.drop(columns=['FOG', 'FOG_LIGHTS', 'FRONT_WIPERS','HEAD_LIGHTS','RAIN', 'REAR_WIPERS', 'SNOW'])
     return x
+
+def extract_data(filenames, X_dict):
+    for i in range(5):
+        for j in range(4):
+            inputs = sample_input(filenames[4*i+j])
+            inputs = torch.Tensor(inputs.to_numpy())
+            
+            split = torch.split(inputs,200) # split into 200 sample arrays
+            stack = torch.stack(list(split)[:int(inputs.shape[0]/200)],dim=0) # stack the tuples into a tensor
+            X_dict[i][j] = stack
+    return X_dict
+
+def split_data(X_dict):
+    X_test = []
+    X_train = []
+    y_test = []
+    y_train = []
+    for i in range(5):
+        for j in range(4):
+            seed = 0 # we can define this randomly
+            if j != 2:
+                X_test.append(X_dict[i][j][seed,:,:].reshape(1,X_dict[i][j].shape[1],X_dict[i][j].shape[2]))
+                X_train.append(X_dict[i][j][seed+1:,:,:])
+            else:
+                X_train.append(X_dict[i][j])
+                
+    return X_test, X_train, y_test, y_train
--- main.py
+++ main.py
@@ -6,19 +6,26 @@ from dataset import helper
 from models.d2v import D2V
 
 def main():
+    filenames = ["user_0001/user_0001_highway.csv", "user_0001/user_0001_suburban.csv", "user_0001/user_0001_tutorial.csv", "user_0001/user_0001_urban.csv", "user_0002/user_0002_highway.csv", "user_0002/user_0002_suburban.csv", "user_0002/user_0002_tutorial.csv", "user_0002/user_0002_urban.csv", "user_0003/user_0003_highway.csv", "user_0003/user_0003_suburban.csv", "user_0003/user_0003_tutorial.csv", "user_0003/user_0003_urban.csv", "user_0004/user_0004_highway.csv", "user_0004/user_0004_suburban.csv", "user_0004/user_0004_tutorial.csv", "user_0004/user_0004_urban.csv", "user_0005/user_0005_highway.csv", "user_0005/user_0005_suburban.csv", "user_0005/user_0005_tutorial.csv", "user_0005/user_0005_urban.csv"]
+    X = {0: {0: [], 1: [], 2: [], 3: []}, 1: {0: [], 1: [], 2: [], 3: []}, 2: {0: [], 1: [], 2: [], 3: []}, 3: {0: [], 1: [], 2: [], 3: []}, 4: {0: [], 1: [], 2: [], 3: []} }
+    y = {0: {0: [], 1: [], 2: [], 3: []}, 1: {0: [], 1: [], 2: [], 3: []}, 2: {0: [], 1: [], 2: [], 3: []}, 3: {0: [], 1: [], 2: [], 3: []}, 4: {0: [], 1: [], 2: [], 3: []} }
+    X_dict = helper.extract_data(filenames, X)
+    
     input_channels = 31
     input_length = 1000
     output_channels = 32
     kernel_size = 16
     dilation_base = 2
+    filename = "user_0001/user_0001_highway.csv"
 
-    inputs = helper.sample_input()
+    inputs = helper.sample_input(filename)
     inputs = torch.Tensor(inputs.to_numpy())
     inputs = inputs.reshape(1, input_channels, input_length)
     splits = torch.split(inputs, input_channels, dim=1)
       
     model = D2V(input_channels, input_length, output_channels, kernel_size, dilation_base)
     model(inputs)
+    
     return
     inputs = splits[0]
     wvlt_inputs = splits[1]
